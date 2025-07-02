222a6fe5085f530b039cde0c92e60f87d3d2c435
Hotto Dogu <ian@takol.tw>
2025-07-02 11:33:46 +0800

    database url

--- app/database.py
+++ app/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from app.models import user
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")  # fallback for local testing
+DATABASE_URL = os.getenv("postgresql://postgres:Xu.6up4u06@db.ikjyahdimpdadsszmgzg.supabase.co:5432/postgres", "sqlite:///./test.db")  # fallback for local testing
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -17,5 +18,4 @@ def get_db():
         db.close()
 
 def create_tables():
-    import app.models.user
     Base.metadata.create_all(bind=engine)
